35024ce5794b03f21c5735c11633943c59702ddf
freeman <2320090860@qq.com>
2021-09-14 12:24:42 +0800

    Initial Commit

---
+++ b站练习/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  b站练习
+//
+//  Created by 葛文轩 on 2021/9/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
